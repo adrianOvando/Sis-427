b1e1433c28b8d82fc833b0a0f3ccdb48f3bd7987
Ovando Jesús Adrián <130518598+adrianOvando@users.noreply.github.com>
2025-03-06 15:01:06 -0400

    .

--- validar.php
+++ validar.php
@@ -9,11 +9,11 @@ $a = $_POST["usuario"];
 $b = $_POST["contrasena"];
 
 if ($a === "admin" && $b === $usuarios["admin"]) {
-    echo "Bienvenido, Administrador. <a href='admin.php'>Ir a Admin</a>";
+    echo "<a href='admin.php'>Ir a Admin</a>";
 } elseif ($a === "user" && $b === $usuarios["user"]) {
-    echo "Bienvenido, Usuario. <a href='user.php'>Ir a Usuario</a>";
+    echo "<a href='user.php'>Ir a Usuario</a>";
 } else {
-    echo "Usuario o contraseña incorrectos. <a href='login.html'>Intentar de nuevo</a>";
+    echo "<a href='login.html'>Intentar de nuevo</a>";
 }
 
 ?>
